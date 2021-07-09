e92e80c6b65928dcf892debf0a2f200e28ec40e2
Michaela Müller <mumichae@in.tum.de>
2021-07-09 23:42:06 +0200

    DeepCirCode with random validation set, L2 regularisation, early stopping

--- workflow/scripts/models/DeepCirCode.py
+++ workflow/scripts/models/DeepCirCode.py
@@ -6,8 +6,10 @@ import pandas as pd
 from keras.models import Sequential
 from keras.layers import Conv1D, MaxPooling1D
 from keras.layers import Dense, Dropout, Flatten
+from keras.regularizers import l2
 from keras import losses
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
     import sys
@@ -15,12 +17,14 @@ if __name__ == "__main__":
     sys.path.append(snakemake.input['common'])
     from common import convert_seqs_to_matrix
 
+    np.random.seed(123)
+
     train_set = pd.read_table(snakemake.input['train_data'].__str__())
-    train_set = train_set[1:10]
     x_train = convert_seqs_to_matrix(train_set['encoded_seq'])
     y_train = keras.utils.to_categorical(train_set['label'], 2)
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2)
 
-    np.random.seed(123)
+    print(f'train: {len(y_train)}\t validation: {len(y_val)}')
 
     model = Sequential()
 
@@ -29,13 +33,17 @@ if __name__ == "__main__":
                          strides=1,
                          padding='valid',
                          activation='relu',
-                         input_shape=(200, 4))
+                         input_shape=(200, 4),
+                         kernel_regularizer=l2(0.01)
+                )
 
     conv_layer2 = Conv1D(filters=128,
                          kernel_size=6,
                          strides=1,
                          padding='valid',
-                         activation='relu')
+                         activation='relu',
+                         kernel_regularizer=l2(0.01)
+                 )
 
     model.add(conv_layer1)
     model.add(Dropout(0))
@@ -53,7 +61,7 @@ if __name__ == "__main__":
 
     model.compile(
         loss=losses.binary_crossentropy,
-        optimizer="RMSprop",
+        optimizer=keras.optimizers.RMSprop(learning_rate=1e-3),
         metrics=['accuracy', keras.metrics.Precision(), keras.metrics.Recall()]
     )
 
@@ -62,7 +70,15 @@ if __name__ == "__main__":
         x_train, y_train,
         batch_size=128,
         epochs=80,
-        validation_split=0.1
+        validation_data=(x_val, y_val),
+        callbacks=[
+            keras.callbacks.EarlyStopping(
+                monitor="val_loss",
+                min_delta=1e-3,
+                patience=3,
+                verbose=1,
+            )
+        ]
     )
 
     df = pd.DataFrame.from_dict(history.history)
